d6bb4bcb514087440c3e32ab8be2cd31fc2ac212
Priyanka Pawar <priyankapawar7764@gmail.com>
2025-12-27 07:01:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5053216381-ccc59",
+  "appId": "1:158760156456:web:de617dab9d4f09aaa9641d",
+  "apiKey": "AIzaSyDUAnBYLyOlPPjQ_mOr8xzWvgWwrWHX30M",
+  "authDomain": "studio-5053216381-ccc59.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "158760156456"
+};
